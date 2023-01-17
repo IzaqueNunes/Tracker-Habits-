07aeecd42af853e3c422975ce5ce69a8462f1fa5
= <=>
2023-01-17 11:53:12 -0300

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { Habit } from "./components/Habit";
+
+import "./styles/global.css";
+
+function App() {
+  return (
+    <div>
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+    </div>
+  );
+}
+
+export default App;
