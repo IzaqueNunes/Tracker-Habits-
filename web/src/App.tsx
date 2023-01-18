cff3f64597cb3d3734b11354ecdb9d3a164021f0
= <=>
2023-01-18 12:38:59 -0300

    create summary

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import { Habit } from "./components/Habit";
-
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 import "./styles/global.css";
 
-function App() {
+export function App() {
   return (
-    <div>
-      <Habit completed={3} />
-      <Habit completed={3} />
-      <Habit completed={3} />
-      <Habit completed={3} />
-      <Habit completed={3} />
-      <Habit completed={3} />
+    <div className="w-screen h-screen flex justify-center items-center bg-background">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   );
 }
-
-export default App;
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.tsx',
-    './index.html'
+    "./App.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#09090A',
+      },
+
+      gridTemplateRows:{
+        7: 'repeat(7, minmax(0, 1fr))'
+      }
+    },
   },
   plugins: [],
 }
